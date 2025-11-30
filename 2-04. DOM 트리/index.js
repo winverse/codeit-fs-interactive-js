6f989ead06ef71dff1759ecfa2b3c1cd4f544f2b
winverse <public.winverse@gmail.com>
2025-11-30 13:50:58 +0900

    chore: 2-05. DOM 트리 여행하기 예제 및 실습 파일 추가

--- 2-04. DOM 트리/index.js
+++ 2-04. DOM 트리/index.js
@@ -1,15 +1,1 @@
 // DOM 트리 실습
-const list = document.querySelector("ul");
-
-// 부모 노드에 접근
-console.log(list.parentNode); // body
-
-// 자식 노드들에 접근
-console.log(list.children); // [li, li]
-
-// 형제 노드에 접근
-console.log(list.previousElementSibling); // p
-console.log(list.nextElementSibling); // script
-
-const heading = document.querySelector("h1");
-console.log(heading.nodeType); // 1 (요소 노드)
